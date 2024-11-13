b07d036209c779a5982e8e34ce2f205ffe55989a
Szymon P <szymon_jozef@proton.me>
2024-11-13 21:43:02 +0100

    changed variable names

--- render.c
+++ render.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 
 void printGameArea() {
-  for (int y = 0; y < size[0]; y++) {
-    for (int x = 0; x < size[1]; x++) {
-      if (x == snakeX[0] && y == snakeY[0]) {
+  for (int row = 0; row < size[0]; row++) {
+    for (int column = 0; column < size[1]; column++) {
+      if (column == snakeX[0] && row == snakeY[0]) {
         printf("O");
-      } else if (x == apple[1] && y == apple[0]) {
+      } else if (column == apple[1] && row == apple[0]) {
         printf("A");
-      } else if (y == 1 || y == size[0] - 1) {
+      } else if (row == 1 || row == size[0] - 1) {
         printf("-");
-      } else if (x == 0 || x == size[1] - 1) {
+      } else if (column == 0 || column == size[1] - 1) {
         printf("|");
       } else {
         int isBody = 0;
         for (int i = 1; i < snake_lenght; i++) {
-          if (x == snakeX[i] && y == snakeY[i]) {
+          if (column == snakeX[i] && row == snakeY[i]) {
             printf("o");
             isBody = 1;
             break;
